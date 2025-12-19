9d2e2e6746c799e286c94e6193a4214dec02a42f
Jan <jansaidasyed743@gmail.com>
2025-12-19 12:11:29 +0530

    feat: Add USD to INR conversion rate constant

--- predictor/utils.py
+++ predictor/utils.py
@@ -2,6 +2,8 @@ import joblib
 import os
 from django.conf import settings
 
+USD_TO_INR = 83.0 # Approx exchange rate
+
 def load_model():
     model_path = os.path.join(settings.BASE_DIR, 'model_linear_reg.pkl')
     if not os.path.exists(model_path):
